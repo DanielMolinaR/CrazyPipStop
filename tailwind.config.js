4d01094a68721b30504649bb147cb08dbdb77635
Daniel Molina <dani99mr31@gmail.com>
2023-06-13 23:42:57 +0200

    Base setup with expo and nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      extend: {    
+        colors: {
+        'cps-red': "#D2160F",
+        'cps-orange': "#E87600",
+        'cps-yellow': "#F7D133",
+        'cps-green': "#8CC63F",
+        'cps-gray': "#3A3A39",
+        'cps-brown': "#562900",
+        'cps-deep-red': "#4f0606"
+        },
+      },
+    },
+    plugins: [],
+  }
+  
