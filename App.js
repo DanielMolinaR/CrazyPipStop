31e3ca6acfa712cdc6253c0ac7a4896541ad76fc
Daniel Molina <dani99mr31@gmail.com>
2023-08-16 22:40:50 +0200

    Trying to apply font

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default function App() {
         hidden={false}
       />
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen key="Home" name='Home' component={HomeScreen} />
+      <Stack.Screen key="Home" name='Home' component={HomeScreen} options={{unmountOnBlur: true}} />
       <Stack.Screen key="Game" name='Game' >{(props) => <GameScreen {...props}/>}</Stack.Screen>
       <Stack.Screen key="Resolve" name='Resolve' >{(props) => <ResolveScreen {...props}/>}</Stack.Screen>
       <Stack.Screen key="ComponentsScreen" name='ComponentsScreen' component={ComponentsScreen} />
